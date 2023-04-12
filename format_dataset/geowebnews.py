1bbb3bb8938982a78fda2a12ea8923bc513fcc68
Zekun Li <zekun@ip-172-31-9-231.us-west-2.compute.internal>
2023-04-12 22:13:15 +0000

    gwn processing

--- format_dataset/geowebnews.py
+++ format_dataset/geowebnews.py
@@ -8,16 +8,18 @@ from bs4 import BeautifulSoup
 import pdb 
 
 IF_WRITE_UNIQUE_NAME = True
-file_path = '../lgl/lgl.xml'
-out_file_path = '../lgl/lgl.json'
+file_path = '../geowebnews/GWN.xml'
+out_file_path = '../geowebnews/GWN.json'
 
 with open(file_path, 'r') as f:
     soup = BeautifulSoup(f, "html.parser")
 
 count_nongaz = 0 
 count_gaz = 0
+count_nogeocoord = 0
 ret_dict_list = []
 unique_names = []
+
 for article in soup.find_all('article'):
     ret_dict = {}
     text = article.find_all('text')[0].text 
@@ -26,30 +28,25 @@ for article in soup.find_all('article'):
     ret_dict['toponyms'] = []
     for toponym in toponyms:
         
+        if toponym.latitude is None:
+            count_nogeocoord += 1
+            continue 
         
         start_pos = int(toponym.start.text)
         end_pos = int(toponym.end.text)
-        phrase = toponym.phrase.text
-        gaztag = toponym.gaztag
+        phrase = toponym.extractedname.text
 
-        topo_dict = {'text':phrase, 'start':start_pos, 'end':end_pos}
-        if gaztag is not None:
-            geonameid = gaztag.get('geonameid')
+        lat = float(toponym.latitude.text)
+        lon = float(toponym.longitude.text)
 
-            flcass = gaztag.fclass.text 
-            fcode = gaztag.fcode.text 
-            lat = float(gaztag.lat.text)
-            lon = float(gaztag.lon.text)
-            if gaztag.country is not None:
-                country_geonameid = gaztag.country.get('geonameid')
-                country_name = gaztag.country.text 
-            if gaztag.admin1 is not None:
-                admin1_geonameid = gaztag.admin1.get('geonameid')
-                admin1_name = gaztag.admin1.text
+        topo_dict = {'text':phrase, 'start':start_pos, 'end':end_pos, 'lat':lat, 'lon':lon}
 
+        geonameid = toponym.geonamesid.text
+        
+
+        if len(geonameid) != 0: 
             topo_dict['geoname_id'] = geonameid
-            topo_dict['lat'] = lat 
-            topo_dict['lon'] = lon 
+            
             unique_names.append(phrase) # list length: 5000, set length: 1886
 
             count_gaz += 1
@@ -57,14 +54,20 @@ for article in soup.find_all('article'):
             count_nongaz += 1 
 
         ret_dict['toponyms'].append(topo_dict) 
+        # pdb.set_trace()
 
     ret_dict_list.append(ret_dict) 
 
 print('count_nongaz:', count_nongaz)
 print('count_gaz:', count_gaz)
+print('count_nogeocoord:', count_nogeocoord)
 
-# count_nongaz: 626
-# gaz: 4462
+
+# count_nongaz: 200
+# count_gaz: 2401
+# count_nogeocoord: 4011
+
+
 
 with open(out_file_path, 'w',  encoding='utf8') as f:
     json.dump(ret_dict_list, f, indent = 2, ensure_ascii = False)
@@ -73,7 +76,7 @@ with open(out_file_path, 'w',  encoding='utf8') as f:
 unique_names = set(unique_names) # list length: 5000, set length: 1886
 
 if IF_WRITE_UNIQUE_NAME:
-    with open('../lgl/unique_names.csv', 'w') as f:
+    with open('../geowebnews/unique_names.csv', 'w') as f:
         # create CSV writer object
         writer = csv.writer(f)
         
